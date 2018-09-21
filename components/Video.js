ff27a69c63569952ac8659c346263326ad59656a
Shrey Gupta <gupta_shrey@live.in>
2018-09-21 10:53:57 +0530

    Check for placeholder image changed

--- components/Video.js
+++ components/Video.js
@@ -377,7 +377,7 @@ class Video extends Component {
       >
         <StatusBar hidden={fullScreen} />
         {
-          ((loading && placeholder) || currentTime < 0.01) &&
+          ((loading && placeholder)) &&
           <Image resizeMode="cover" style={styles.image} {...checkSource(placeholder)} />
         }
         <VideoPlayer
